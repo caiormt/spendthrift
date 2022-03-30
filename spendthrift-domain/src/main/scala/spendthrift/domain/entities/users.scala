19a8995b612938e07a7fbb8fc467879899d5e6da
Caio Tokunaga <caiormt@gmail.com>
2022-03-29 21:49:48 -0400

    feat(user): creating user domain

---
+++ spendthrift-domain/src/main/scala/spendthrift/domain/entities/users.scala
@@ -0,0 +1,16 @@
+package spendthrift.domain.entities
+
+import cats.*
+import cats.derived.*
+
+import java.util.*
+
+object users:
+
+  opaque type UserId = UUID
+
+  // format: off
+  final case class User(
+      id: UserId
+  ) derives Eq, Show
+  // format: on
